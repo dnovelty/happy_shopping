6cd0b013bda6b3ab558b1201fde267304f502f05
dnovelty <dignovelty@gmail.com>
2021-02-10 20:33:48 +0800

    删除测试代码

--- happy_shopping/httpUtils.py
+++ happy_shopping/httpUtils.py
@@ -12,13 +12,3 @@ def callback_2_json(s):
 
 def status_ok(resp):
     return resp.status == requests.codes.OK
-
-
-from urllib import request
-from http.cookiejar import MozillaCookieJar
-
-cookiejar = MozillaCookieJar('cookie.txt')
-handler = request.HTTPCookieProcessor(cookiejar)
-opener = request.build_opener(handler)
-resp = opener.open('http://www.baidu.com')
-cookiejar.save()
